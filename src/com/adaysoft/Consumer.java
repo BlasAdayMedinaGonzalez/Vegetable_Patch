ae4709d03bd03742427c35228fd240892c6a119a
BlasAdayMedinaGonzalez <usr2702@salesianos-lacuesta.net>
2021-11-16 13:28:57 +0000

    Creación de variables y creación de funciones en la clase Consumer, que recoge cada vegetal del buffer.get() de cada espacio del huerto

--- src/com/adaysoft/Consumer.java
+++ src/com/adaysoft/Consumer.java
@@ -1,5 +1,27 @@
 package com.adaysoft;
 
 public class Consumer extends Thread{
+    private VegetablePatchMonitor buffer;
+    private int expectedUnits;
+    private int maxTimeForConsume;
 
+    public Consumer(VegetablePatchMonitor buffer, int expectedUnits, int maxTimeForConsume) {
+        this.buffer = buffer;
+        this.expectedUnits = expectedUnits;
+        this.maxTimeForConsume = maxTimeForConsume;
+    }
+
+    @Override
+    public void run() {
+        String vegetable;
+        try {
+            for (int i = 0; i < expectedUnits; i++) {
+                vegetable = buffer.get();
+                System.out.println("Consumed -> " + vegetable);
+                sleep((int) (Math.random() * maxTimeForConsume));
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 }
