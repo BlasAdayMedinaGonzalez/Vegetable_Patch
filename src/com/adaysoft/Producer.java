9a322ed3f350e46983e8520fb0a2271944899e95
BlasAdayMedinaGonzalez <usr2702@salesianos-lacuesta.net>
2021-11-16 13:27:27 +0000

    Creación de variables y creación de funciones en la clase Producer, que genera/produce aleatorio los vegetales

--- src/com/adaysoft/Producer.java
+++ src/com/adaysoft/Producer.java
@@ -1,5 +1,46 @@
 package com.adaysoft;
 
 public class Producer extends Thread{
+    private VegetablePatchMonitor buffer;
 
+    private int produceUnits;
+    private int maxTimeForProduce;
+
+    private static String vegetables [] = {
+            "lettuce",
+            "cabbage",
+            "onion",
+            "spinach",
+            "potato",
+            "celery",
+            "asparagus",
+            "radish",
+            "broccoli",
+            "artichoke",
+            "tomato",
+            "cucumber",
+            "eggplant",
+            "carrot",
+            "green bean"
+    };
+
+    public Producer(VegetablePatchMonitor buffer, int produceUnits, int maxTimeForProduce) {
+        this.buffer = buffer;
+        this.produceUnits = produceUnits;
+        this.maxTimeForProduce = maxTimeForProduce;
+    }
+
+    @Override
+    public void run() {
+        try {
+            for (int i = 0; i < produceUnits; i++) {
+                int randomNum = (int) (Math.random() * vegetables.length);
+                buffer.put(vegetables[randomNum]);
+                System.out.println("Produced -> " + vegetables[randomNum]);
+                sleep((int) (Math.random() * this.maxTimeForProduce));
+            }
+        } catch (InterruptedException e) {
+            e.printStackTrace();
+        }
+    }
 }
